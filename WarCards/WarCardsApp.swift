c8c5f96a7b14550d975cb1d0f222c365c14e667b
Eugene <contact.duk@gmail.com>
2022-02-20 17:23:54 +0300

    Adding initial source code

---
+++ WarCards/WarCardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarCardsApp.swift
+//  WarCards
+//
+//  Created by Eugene on 22.11.21.
+//
+
+import SwiftUI
+
+@main
+struct WarCardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
